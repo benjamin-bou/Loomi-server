a5342febcc50c0bcfbb87219022d406735d965d0
Benjamin.Boutrois <benjamin.boutrois@my-digital-school.org>
2025-03-28 11:49:41 +0100

    feat(order) : Improved order - Improved factory - Improved migration - Improved seeder

--- database/factories/OrderFactory.php
+++ database/factories/OrderFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\User;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Order>
@@ -17,7 +18,10 @@ class OrderFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'user_id' => User::inRandomOrder()->first()->id,
+            'order_number' => $this->faker->unique()->numerify('ORD-#####'),
+            'total_amount' => $this->faker->randomFloat(2, 10, 1000),
+            'status' => $this->faker->randomElement(['pending', 'paid', 'delivered', 'received', 'completed', 'canceled']),
         ];
     }
 }
--- database/migrations/2025_03_28_102028_create_order_table.php
+++ database/migrations/2025_03_28_102028_create_order_table.php
@@ -11,7 +11,14 @@ return new class extends Migration
      */
     public function up(): void
     {
-        //
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('order_number')->unique();
+            $table->decimal('total_amount', 10, 2);
+            $table->string('status')->default('pending');
+            $table->timestamps();
+        });
     }
 
     /**
