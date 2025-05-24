59ada213a29bf5ccac38385887ac8e35f56508bd
Benjamin.Boutrois <benjamin.boutrois@my-digital-school.org>
2025-05-24 15:02:59 +0200

    feat(subscription) : Created subscription get route

--- routes/api.php
+++ routes/api.php
@@ -11,6 +11,7 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:api')->group(function () {
     Route::get('/profile', [AuthController::class, 'me']);
     Route::get('/orders', [OrderController::class, 'index']);
+    // Route::get('/subscription', [SubscriptionController::class, 'index']);
 
     Route::get('/admin/boxes', [BoxController::class, 'adminIndex']);
     Route::put('/admin/boxes/{id}', [BoxController::class, 'update']);
