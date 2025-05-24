f37b188744fb18d032d6f1f5af87cb15db9d9974
Benjamin.Boutrois <benjamin.boutrois@my-digital-school.org>
2025-05-24 11:36:03 +0200

    feat(user) : Added profile route

--- routes/api.php
+++ routes/api.php
@@ -8,7 +8,7 @@ Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:api')->group(function () {
-    Route::get('/user', [AuthController::class, 'me']);
+    Route::get('/profile', [AuthController::class, 'me']);
     Route::get('/admin/boxes', [BoxController::class, 'adminIndex']);
     Route::put('/admin/boxes/{id}', [BoxController::class, 'update']);
 });
