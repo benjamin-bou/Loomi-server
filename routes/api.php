0ca285160d26981dc9970699ff9414f525de051d
Benjamin.Boutrois <benjamin.boutrois@my-digital-school.org>
2025-05-26 07:56:12 +0200

    feat(subscription): Move subscription routes outside auth middleware for better accessibility

--- routes/api.php
+++ routes/api.php
@@ -9,15 +9,16 @@ use App\Http\Controllers\SubscriptionController;
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
+Route::get('/subscriptions', [SubscriptionController::class, 'index']);
+Route::get('/subscriptions/{id}', [SubscriptionController::class, 'show']);
+
 Route::middleware('auth:api')->group(function () {
     Route::get('/profile', [AuthController::class, 'me']);
     Route::get('/orders', [OrderController::class, 'index']);
-    Route::get('/subscriptions', [SubscriptionController::class, 'index']);
-    Route::get('/subscriptions/{id}', [SubscriptionController::class, 'show']);
     Route::get('/my-subscription', [SubscriptionController::class, 'current']);
     Route::get('informations', [AuthController::class, 'me']);
-
     Route::post('/order', [OrderController::class, 'store']);
+
 
     Route::get('/admin/boxes', [BoxController::class, 'adminIndex']);
     Route::put('/admin/boxes/{id}', [BoxController::class, 'update']);
