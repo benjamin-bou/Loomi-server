f89707c169eccfd7353534bc1840226bb902ff8a
Benjamin.Boutrois <benjamin.boutrois@my-digital-school.org>
2025-05-29 16:31:11 +0200

    feat(delivery): Implement subscription delivery management and scheduling

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\BoxController;
 use App\Http\Controllers\GiftCardController;
 use App\Http\Controllers\OrderController;
 use App\Http\Controllers\SubscriptionController;
+use App\Http\Controllers\DeliveryController;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
@@ -21,6 +22,7 @@ Route::get('payment-methods', [OrderController::class, 'getPaymentMethods']);
 Route::middleware('auth:api')->group(function () {
     Route::get('/profile', [AuthController::class, 'me']);
     Route::get('/orders', [OrderController::class, 'index']);
+    Route::get('/profile/deliveries', [DeliveryController::class, 'getUserDeliveries']);
     Route::get('/my-subscription', [SubscriptionController::class, 'current']);
     Route::post('/cancel-subscription', [SubscriptionController::class, 'cancel']);
     Route::get('informations', [AuthController::class, 'me']);
@@ -30,6 +32,10 @@ Route::middleware('auth:api')->group(function () {
     Route::post('/profile', [AuthController::class, 'updateProfile']);
 
     Route::get('/my-gift-cards', [GiftCardController::class, 'getUserGiftCards']);
+
+    // Routes pour la gestion des livraisons d'abonnement
+    Route::post('/subscription-deliveries', [DeliveryController::class, 'addSubscriptionDelivery']);
+    Route::patch('/subscription-deliveries/{id}/delivered', [DeliveryController::class, 'markAsDelivered']);
 
     Route::get('/admin/boxes', [BoxController::class, 'adminIndex']);
     Route::put('/admin/boxes/{id}', [BoxController::class, 'update']);
